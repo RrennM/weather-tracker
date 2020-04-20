b66d80f176ee1172364f8075925386a9cd383b33
Tyson Hood <TysonHood@gmail.com>
2020-04-20 06:25:18 -0700

    Get nav elements set up visually.

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import logo from './thicon10.png';
+import './Navigation.css';
 
 const Navigation = () => {
     return (
-        <h1>Helloooo</h1>
+        <nav>
+            <div className='logoContainer'>
+                <img src={logo} alt='Logo' />   
+            </div>
+                <div className='navLinkContainer' style={{display: 'flex', justifyContent: 'flex-end'}}>
+                    <p className='f3 link dim black underline pa3 pointer'>Home</p>
+                    <p className='f3 link dim black underline pa3 pointer'>Sign In</p>
+                </div>
+        </nav>
     )
 }
 
